1a8ac8594e072d55561bbf91305cab198ddbc5a9
Vatsal Patel <vatsal.patel@capitsign.com>
2023-08-20 17:49:44 +0530

    Update logs for missing .env

--- backend/main.go
+++ backend/main.go
@@ -17,7 +17,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	config.InitGoogleOAuth()
